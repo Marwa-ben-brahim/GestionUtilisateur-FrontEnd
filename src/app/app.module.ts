81070a9ba720d11adf4cb23ef404641df1cfb4aa
Basma Triki <Bassmabtriki@gmail.com>
2018-04-09 16:59:54 +0100

    commit msg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
 import { AboutComponent } from './about/about.component';
@@ -10,15 +8,15 @@ import {RouterModule, Routes} from '@angular/router';
 import {HttpModule} from '@angular/http';
 import {UsersServices} from '../services/users.services';
 import { NewUserComponent } from './new-user/new-user.component';
-import { NouveauUserComponent } from './nouveau-user/nouveau-user.component';
 import { EditUserComponent } from './edit-user/edit-user.component';
+import { LoginComponent } from './login/login.component';
 
 const appRoutes:Routes=[
-  {path:'about',component:AboutComponent},
+  {path:'login',component:LoginComponent},
   {path:'users',component:UsersComponent},
   {path:'new-user',component:NewUserComponent},
   {path:'editUser/:id',component:EditUserComponent},
-  {path:'',redirectTo:'/about',pathMatch:'full'}
+  {path:'',redirectTo:'/login',pathMatch:'full'}
 
 ];
 @NgModule ({
@@ -27,8 +25,8 @@ const appRoutes:Routes=[
     UsersComponent,
     AboutComponent,
     NewUserComponent,
-    NouveauUserComponent,
-    EditUserComponent
+    EditUserComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule, RouterModule.forRoot(appRoutes),HttpModule,FormsModule
