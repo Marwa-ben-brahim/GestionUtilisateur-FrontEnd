12c51ddb683c09372e0f1a76f9fe17ac190978be
Marwa Ben Brahim <marwabenbrahim@hotmail.fr>
2018-04-14 02:16:57 +0100

    commit 3

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import {UsersServices} from "../../services/users.services";
+import {ActivatedRoute, Router} from "@angular/router";
+import {User} from "../../model/model.user";
+import {Http} from "@angular/http";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  login:string="";
+  motpasse:string="";
+  currentPage:number=0;
+  pages:Array<number>;
+  size:number=5;
+  pageUsers:any;
+  constructor(public http:Http,public userService:UsersServices,
+              public router:Router) { }
+
+  ngOnInit() {
+  }
+  doSearch(){
+    this.userService.isUser(this.login,this.motpasse)
+      .subscribe(data=>{
+        alert("Success de s'authentifier");
+        this.router.navigate(['users']);
+        console.log(data)
+      },err=>{
+        console.log(err);
+      })
+  }
+  chercheUser(){
+    this.doSearch();
+  }
+}
