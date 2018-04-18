4145844a8d572220458b4a6c12ff768d3274fc46
Marwa Ben Brahim <marwabenbrahim@hotmail.fr>
2018-04-19 00:11:32 +0100

    commit 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
@@ -11,14 +11,19 @@ import { NewUserComponent } from './new-user/new-user.component';
 import { EditUserComponent } from './edit-user/edit-user.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { IndexComponent } from './index/index.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
 
 const appRoutes:Routes=[
   {path:'login',component:LoginComponent},
   {path:'users',component:UsersComponent},
   {path:'new-user',component:NewUserComponent},
   {path:'editUser/:login',component:EditUserComponent},
+  {path:'navbar',component:NavbarComponent},
+  {path:'index',component:IndexComponent},
   {path:'',redirectTo:'/login',pathMatch:'full'}
-
 ];
 @NgModule ({
   declarations: [
@@ -28,11 +33,18 @@ const appRoutes:Routes=[
     NewUserComponent,
     EditUserComponent,
     LoginComponent,
-    DashboardComponent
+    DashboardComponent,
+    IndexComponent,
+    NavbarComponent,
+    FooterComponent,
+    SideBarComponent
   ],
   imports: [
-    BrowserModule, RouterModule.forRoot(appRoutes),HttpModule,FormsModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    HttpModule,FormsModule
   ],
+  schemas:[NO_ERRORS_SCHEMA],
   providers: [UsersServices],
   bootstrap: [AppComponent]
 })
